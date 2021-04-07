7e0ed045e033619f2cc54e2ce84c489cb2849ff9
Kevin Harrington <harrington@wpi.edu>
2021-04-07 15:56:03 -0400

    working demo code

--- subtrans.groovy
+++ subtrans.groovy
@@ -1,1 +1,33 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Transform
+import eu.mihosoft.vrl.v3d.Vector3d
+double d=15
+double rot =5
+
+Vector3d point = Vector3d.xyz(20,20, 0);
+
+CSG cube  = new Cube(	point.x*2,// X dimention
+			point.y*2,// Y dimention
+			5//  Z dimention
+			).toCSG()
+
+println "Point starts at: "+point
+
+Transform frame2 = new Transform().rotz(-rot).movey(d)
+
+CSG cube2=cube.transformed(frame2)
+
+Vector3d point2 = point.transformed(frame2)
+println "Point2 starts at: "+point2+" from "+point
+double offseCalculated = -0.75*point2.getY()
+println "Offset calculated "+offseCalculated
+
+Transform frame3 = frame2.movey(offseCalculated)
+
+CSG cube3=cube2.movey(offseCalculated)
+
+return cube3
+	
+	
+	
